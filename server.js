5a7e46bc91726b4d8632665d1df44f3f6b383d88
Cristian Vargas <cristian.v0223@gmail.com>
2022-07-26 11:52:21 -0500

    added seed data, and adjusted models so name and description are needed to post

--- server.js
+++ server.js
@@ -1,16 +1,13 @@
-//Required Packages
-const express = require('express');
 const path = require('path');
-const routes = require('./controllers');
-const exphbs = require('express-handlebars');
-const helpers = require('./utils/helpers');
+const express = require('express');
 const session = require('express-session');
+const exphbs = require('express-handlebars');
+const routes = require('./controllers');
+const helpers = require('./utils/helpers');
 
-//Sequelize and Session-Sequelize Package
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
-//Initializing app setting it equal to Express method. 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -18,13 +15,13 @@ const PORT = process.env.PORT || 3001;
 const hbs = exphbs.create({ helpers });
 
 const sess = {
-    secret: 'Super secret secret',
-    cookie: {},
-    resave: false,
-    saveUninitialized: true,
-    store: new SequelizeStore({
-      db: sequelize
-    })
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
 };
 
 app.use(session(sess));
@@ -33,7 +30,6 @@ app.use(session(sess));
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
-//Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -41,5 +37,5 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
-    app.listen(PORT, () => console.log('Now listening'));
-  });
+  app.listen(PORT, () => console.log('Now listening'));
+});
