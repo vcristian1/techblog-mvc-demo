eee0476b19e2a8f10526c7f7193f5b42b76775bc
Cristian Vargas <cristian.v0223@gmail.com>
2022-07-27 13:30:56 -0500

    added commentRoutes to index.js in /controllers/api/index.js

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const projectRoutes = require('./projectRoutes');
+const commentRoutes = require('./commentRoutes')
 
 router.use('/users', userRoutes);
+router.use('/comments', commentRoutes);
 router.use('/projects', projectRoutes);
 
 module.exports = router;
